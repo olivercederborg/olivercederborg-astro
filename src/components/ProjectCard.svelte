507c3066ec1b1eb33795e18860e4e471df82dfa8
Oliver Cederborg <hi@olivercederborg.com>
2022-01-19 23:11:03 +0100

    feat: update ProjectCard component

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-	import { Project } from '../types/project'
+	interface Props {
+		project: {
+			url: string
+			title: string
+			thumbnail: string
+			thumbnailAlt: string
+		}
+	}
 
-	export let project: Project
+	export let project: Props['project']
 </script>
 
 <a href="{project.url}">
-	<figure class="relative group">
+	<figure class="relative group aspect-[4/3]">
 		<div
 			class="absolute inset-0 group-hover:bg-primary-brand/80 opacity-0 group-hover:opacity-100 flex flex-col items-center justify-center transition duration-300 ease-in-out"
 		>
@@ -15,6 +22,6 @@
 				{project.title} &rarr;
 			</h3>
 		</div>
-		<img src="{project.thumbnail}" alt="{project.title}" />
+		<img src="{project.thumbnail}" alt="{project.thumbnailAlt}" class="object-cover w-full h-full" />
 	</figure>
 </a>
