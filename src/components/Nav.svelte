9c3e4c9178f089216689d68a4c7e5dd6473db864
Oliver Cederborg <hi@olivercederborg.com>
2022-01-19 23:10:47 +0100

    feat: update Nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,10 +1,14 @@
-<nav class="w-full h-32 border-b-2 border-stone-800">
-<section class="container flex justify-between items-center h-full font-bold uppercase">
-		<img src="/assets/logo.svg" alt="" width="64">
+<script lang="ts">
+	import { fade } from 'svelte/transition'
+</script>
+
+<nav in:fade class="w-full h-32">
+	<section class="container flex justify-between items-center h-full font-bold uppercase">
+		<img src="/assets/logo.svg" alt="Oliver Cederborg logo" width="56" />
 		<ul class="flex gap-x-6">
-			<li><a href="#work">Work</a></li>
-			<li><a href="#about">About</a></li>
-			<li><a href="#contact">Contact</a></li>
+			<li><a href="/#projects">Projects</a></li>
+			<li><a href="/#about">About</a></li>
+			<li><a href="/#contact">Contact</a></li>
 		</ul>
-</section>
+	</section>
 </nav>
