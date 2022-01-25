98ac8dcbbb6606a167f6dceb550ff2c0ea629806
Oliver Cederborg <hi@olivercederborg.com>
2022-01-25 01:04:26 +0100

    feat: add serverless functions

---
+++ api/send.ts
@@ -0,0 +1,32 @@
+const sgMail = require('@sendgrid/mail')
+
+export default async function handler(req, res) {
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+	const { name, email, message } = req.body
+
+	const emailToFrom = {
+		to: 'hey@olivercederborg.com',
+		from: email
+	}
+
+	const content = {
+		to: emailToFrom.to,
+		from: emailToFrom.from,
+		subject: `New Message From: ${name}`,
+		text: message,
+		html: `<p><strong>Name: ${name}<br>
+          Email: ${email}</strong>
+          <br><br>
+          Message: ${message}
+          </p>`
+	}
+
+	try {
+		await sgMail.send(content)
+		res.status(200).send('Message sent successfully.')
+	} catch (error) {
+		console.log('ERROR', error)
+		res.status(400).send('Message not sent.')
+	}
+}
